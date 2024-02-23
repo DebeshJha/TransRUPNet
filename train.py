eb7ec91a1f34cd607fd631d0c0b65899e5a23714
Debesh Jha <36955587+DebeshJha@users.noreply.github.com>
2024-02-23 16:13:14 -0600

    Update train.py

--- train.py
+++ train.py
@@ -184,8 +184,8 @@ if __name__ == "__main__":
     lr = 1e-4
     early_stopping_patience = 50
     checkpoint_path = "files/checkpoint.pth"
-    path = "/media/nikhil/Seagate Backup Plus Drive/ML_DATASET/Kvasir-SEG"
-    # path = "/home/nikhilroxtomar/lab/DATA/Kvasir-SEG"
+    path = "/home/Debesh/Document/Datasets/Kvasir-SEG"
+    # path = "/home/Debesh/Document/Datasets/Kvasir-SEG"
 
     data_str = f"Image Size: {size}\nBatch Size: {batch_size}\nLR: {lr}\nEpochs: {num_epochs}\n"
     data_str += f"Early Stopping Patience: {early_stopping_patience}\n"
